31e956308adb8df5fc1efbc2605e5cc56751a405
Abdallahdalvi <abdallah@dalvi.cloud>
2026-03-31 10:56:54 +0530

    fix: resolve tsconfig and vite config errors for docker build

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthWrapper>
-      {(user) => <App user={user} />}
-    </AuthWrapper>
+    <App />
   </React.StrictMode>,
 )
